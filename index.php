4c7106d917a36663c27c6fc3ed00ca779c148faf
kelic <kelic@Jev.Home>
2019-03-01 16:26:03 +0000

    proj4

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "i want to masturbate"
+        echo "internal bloodthirst"
         ?>
     </body>
 </html>
